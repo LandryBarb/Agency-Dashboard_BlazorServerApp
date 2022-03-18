34e5f9ff0e30cd735aae09059c5d7195c3ad97ca
Landry <landry@landrybarb.com>
2022-03-18 15:02:30 -0700

    Implented Custom methods on class

--- BlazorServerAgencyDashboard/Data/Client.cs
+++ BlazorServerAgencyDashboard/Data/Client.cs
@@ -9,6 +9,7 @@
         public int? Age { get; set; }
         public DateTime? Birthdate { get; set; }
         public bool HasHeadShots { get; set; } = false;
+        //public bool isMinor { get; set; } = false;
 
         public bool CanSubmit()
         {
@@ -29,8 +30,43 @@
         //{
         //    int year = DateTime.Now.Year;
         //    int age = year - (int)(Birthdate?.Year);
+
         //    return age;
         //}
+
+        public int GetAge()
+        {
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - Birthdate.Value.Year;
+            Age = age;
+            return (int)Age;
+        }
+
+        public bool ClientIsMinor()
+        {
+            int currentYear = DateTime.Now.Year;
+
+
+            int birthyear = Convert.ToInt32(Birthdate.Value.Year);
+            Age = currentYear - birthyear;
+
+
+            if (Age < 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+        public int ShowAge()
+        {
+            int calcAge = (int)Age;
+            return calcAge;
+        }
 
     }
 }
