925eccd2fd79a51f9acc405dce429a1b140b752e
Landry <landry@landrybarb.com>
2022-03-31 08:41:39 -0700

    Still working on layout

--- BlazorServerAgencyDashboard/Data/DataContext.cs
+++ BlazorServerAgencyDashboard/Data/DataContext.cs
@@ -16,38 +16,38 @@ namespace BlazorServerAgencyDashboard.Data
                 new Client
                 {
                     Id = 1,
-                    FirstName = "Landry",
-                    LastName = "Barb",
+                    FirstName = "Jack",
+                    LastName = "Jones",
                     Age = 37,
                     Birthdate = new DateTime(1985, 5, 28),
                     Email = "somemail@email.com",
-                    HasHeadShots = false,
-                    ImageUrl = "./images/headshots/person1.jpg"
+                    HasHeadShots = true,
+                    ImageUrl = "./images/headshots/hs1.jpg"
 
                 },
                   new Client
                   {
                       Id = 2,
-                      FirstName = "Jessica",
-                      LastName = "Haganey",
+                      FirstName = "Cyndy",
+                      LastName = "Mannal",
                       Age = 33,
                       Birthdate = new DateTime(1989, 4, 29),
                       Email = "testemail@aol.com",
                       HasHeadShots = true,
-                      ImageUrl = "./images/headshots/person2.jpg"
+                      ImageUrl = "./images/headshots/hs2.jpg"
 
 
                   },
                    new Client
                    {
                        Id = 3,
-                       FirstName = "Loren",
+                       FirstName = "Lauren",
                        LastName = "Salgado",
                        Age = 42,
                        Birthdate = new DateTime(1980, 8, 10),
                        Email = "anewemail@outlook.com",
-                       HasHeadShots = false,
-                       ImageUrl = "./images/headshots/person3.jpg"
+                       HasHeadShots = true,
+                       ImageUrl = "./images/headshots/hs3.jpg"
 
 
                    },
@@ -56,14 +56,27 @@ namespace BlazorServerAgencyDashboard.Data
                         Id = 4,
                         FirstName = "Reef",
                         LastName = "Downes",
-                        Age = 10,
+                        Age = 18,
                         Birthdate = new DateTime(2012, 3, 20),
                         Email = "reefdowns@gmail.com",
                         HasHeadShots = true,
-                        ImageUrl = "./images/headshots/person4.jpg"
+                        ImageUrl = "./images/headshots/hs4.jpg"
 
 
-                    }
+                    },
+                      new Client
+                      {
+                          Id = 5,
+                          FirstName = "James",
+                          LastName = "Cantor",
+                          Age = 10,
+                          Birthdate = new DateTime(2012, 3, 20),
+                          Email = "reefdowns@gmail.com",
+                          HasHeadShots = true,
+                          ImageUrl = "./images/headshots/hs5.jpg"
+
+
+                      }
                 );
         }
         public DbSet<Client> Clients => Set<Client>();
